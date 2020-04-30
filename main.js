126a84af595bca6362372956790591181ac25ac3
mt <margarettom6@gmail.com>
2020-04-30 09:10:35 +0300

    Day 2 - The Vue Instance

--- main.js
+++ main.js
@@ -2,6 +2,16 @@ new Vue ({
     el: '#app',
     data: {
         greeting: 'Hello Vue!',
+        user: 'M-tee',
+        city: 'Nairobi',
+    },
+    methods: {
+        changeGreeting() {
+            this.greeting = this.greeting === 'Hello Vue!' ?
+             'What\'s up!' :
+             'Hello Vue!';
+          }
+
     },
 
 });
