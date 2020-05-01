a10b3d3bd70f91e049c1bef82e7522f2de62b90d
mt <margarettom6@gmail.com>
2020-05-01 08:59:51 +0300

    day 3 & 4  v-on

--- main.js
+++ main.js
@@ -4,13 +4,21 @@ new Vue ({
         greeting: 'Hello Vue!',
         user: 'M-tee',
         city: 'Nairobi',
+        alertMessage: ''
     },
     methods: {
         changeGreeting() {
             this.greeting = this.greeting === 'Hello Vue!' ?
              'What\'s up!' :
              'Hello Vue!';
-          }
+          },
+          alertMessageGreeting() {
+            this.alertMessage = 'You typed something!';
+          },
+          alertEnterGreeting() {
+            this.alertMessage = 'You typed and pressed Enter!';
+          },
+
 
     },
 
