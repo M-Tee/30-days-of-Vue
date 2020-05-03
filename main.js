3859024a316b7776150e1563cddf69fdba917ba5
mt <margarettom6@gmail.com>
2020-05-03 13:22:49 +0300

    v- model

--- main.js
+++ main.js
@@ -66,3 +66,24 @@ new Vue({
     }
   }
 });
+
+new Vue({
+  el: '#form',
+  data: {
+    name: '',
+    subject: '',
+    termsAndConditions: false,
+    yesOrNo: 'No'
+  },
+  methods: {
+    submit() {
+      console.log('name', this.name);
+      console.log('subject', this.subject);
+      console.log(
+        'termsAndConditions',
+        this.termsAndConditions
+      );
+      console.log('yesOrNo', this.yesOrNo);
+    }
+  }
+});
