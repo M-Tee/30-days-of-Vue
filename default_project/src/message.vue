72c27dea467ab60bcd32c2ac45b9e60cb514711d
mt <margarettom6@gmail.com>
2020-05-21 11:48:55 +0300

    Linking components globally

---
+++ default_project/src/message.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h2> {{message}} </h2>
+        <app-input :msg= "message"></app-input>
+    </div>
+</template>
+
+<script>
+import input from './input.vue';
+
+export default {
+    data(){
+        return{
+            message: 'This is the message!'
+        }
+    },
+    components:{
+        'app-input': input
+    }
+}
+</script>
